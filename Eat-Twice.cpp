cf0c8addaa796c671a22a40e253c539558e8166f
KevinThelly <kevin.thelly@gmail.com>
2019-06-23 23:03:28 +0530

    JUNE COOK OFF 2019

--- Eat-Twice.cpp
+++ Eat-Twice.cpp
@@ -1,22 +1,50 @@
 #include <iostream>
 using namespace std;
 
-void selectionSort(int a[], int n,int d[]) {
-   int i, j, min, temp,temp2;
-   for (i = 0; i < n - 1; i++) {
-      min = i;
-      for (j = i + 1; j < n; j++)
-         if (a[j] < a[min])
-            min = j;
-      temp = a[i];
-      a[i] = a[min];
-      a[min] = temp;
-
-      temp2=d[i];
-      d[i]=d[min];
-      d[min]=temp2;
-   }
+void swap(int* a, int* b) 
+{ 
+    int t = *a; 
+    *a = *b; 
+    *b = t; 
 }
+
+int partition (int arr[], int low, int high,int d[]) 
+{ 
+    int pivot = arr[high];    // pivot 
+    int i = (low - 1);  // Index of smaller element 
+  
+    for (int j = low; j <= high- 1; j++) 
+    { 
+        // If current element is smaller than or 
+        // equal to pivot 
+        if (arr[j] <= pivot) 
+        { 
+            i++;    // increment index of smaller element 
+            swap(&arr[i], &arr[j]);
+            swap(&d[i],&d[j]);
+        } 
+    } 
+    swap(&arr[i + 1], &arr[high]); 
+    swap(&d[i + 1], &d[high]); 
+    return (i + 1); 
+} 
+  
+
+
+void quickSort(int arr[], int low, int high,int d[]) 
+{ 
+    if (low < high) 
+    { 
+        /* pi is partitioning index, arr[p] is now 
+           at right place */
+        int pi = partition(arr, low, high,d); 
+  
+        // Separately sort elements before 
+        // partition and after partition 
+        quickSort(arr, low, pi - 1,d); 
+        quickSort(arr, pi + 1, high,d); 
+    } 
+} 
 
 int main() {
   int t;
@@ -30,14 +58,15 @@ int main() {
     for(int j=0;j<n;j++) {
       cin>>days[j]>>v[j];      
     }
-    selectionSort(v,n,days);
+    quickSort(v,0,n,days);
     
-    int same=days[n-1];
-    int j=n-2;
+    int same=days[n];
+    int j=n-1;
     while(same==days[j]){
       j=j-1;
     }
-    int best=v[n-1]+v[j];
+
+    int best=v[n]+v[j];
     cout<<best<<endl;
   }
   return 0;
